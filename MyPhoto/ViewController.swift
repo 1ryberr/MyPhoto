34eb1cea114c31a431d7ae7a01bd42f03b93fc3d
Ryan Berry <ryanberry@ryans-imac.lan>
2018-04-27 09:39:17 -0400

    Initial Commit

---
+++ MyPhoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyPhoto
+//
+//  Created by Ryan Berry on 4/27/18.
+//  Copyright © 2018 Ryan Berry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
